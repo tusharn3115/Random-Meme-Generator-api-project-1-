4ca5ff8b4697f47803c3ddc34037a4ba2858c01f
Tushar Negi <negitushar81@gmail.com>
2024-09-13 13:36:58 +0530

    api calling

---
+++ Recipe App Api/script.js
@@ -0,0 +1,37 @@
+const searchBox = document.querySelector("#searchBox");
+const searchButton = document.querySelector("#searchBtn");
+const recipeContainer = document.querySelector(".recipe-container");
+
+
+// getting recipes
+const getRecipes = async(input) => {
+
+    // return promise
+    const data = await fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
+
+    // convert into JSON format
+    const response = await data.json();
+    // console.log(response.meals[0]);
+
+    response.meals.forEach(meal => {
+        // console.log(meal);
+
+        // creating HTML element 
+        const showRecipe = document.createElement('div');
+        showRecipe.classList.add('recipe');
+        showRecipe.innerHTML = `
+          <img src="${meal.strMealThumb}">
+          <h3>${meal.strMeal}</h3> 
+          <p>${meal.strArea}</p> 
+          <p>${meal.strCategory}</p> 
+        `
+
+        // adding the recipe details with photo into the recipeContainer 
+        recipeContainer.appendChild(showRecipe);
+    });
+}
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();    // stoped for auto-submission
+    const searchInput = searchBox.value.trim();
+    getRecipes(searchInput);
+})
